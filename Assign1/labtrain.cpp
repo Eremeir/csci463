87df736f8de686441cd8c4c4a3760e19b3a504ab
Matt Borek <z1951125@students.niu.edu>
2022-09-30 01:52:41 -0500

    Add Assign1

---
+++ Assign1/labtrain.cpp
@@ -0,0 +1,20 @@
+//******************************************************************
+//
+// CSCI 463 Assignment 1
+//
+// Author: Matt Borek
+//
+// Unix Training Exercise
+//
+//******************************************************************
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
