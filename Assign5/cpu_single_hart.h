b33bec91ecf1c4f7f28a16325857f899498f7909
Matt Borek <z1951125@students.niu.edu>
2022-10-13 22:06:49 -0500

    Impliment CPU header

--- Assign5/cpu_single_hart.h
+++ Assign5/cpu_single_hart.h
@@ -11,6 +11,14 @@
 //  of the starter code provided for the assignment.
 //
 //***************************************************************************
+#include "rv32i_hart.h"
 
+class cpu_single_hart : public rv32i_hart
+{
+public:
+    cpu_single_hart(memory &mem) : rv32i_hart(mem) {}
+    void run(uint64_t exec_limit);
+
+};
 
 #endif
