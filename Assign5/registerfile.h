44686c004b917adfb28819458694cc9a3ea4bea0
Matt Borek <z1951125@students.niu.edu>
2022-10-14 02:42:27 -0500

    Impliment initial registerfile functions.

--- Assign5/registerfile.h
+++ Assign5/registerfile.h
@@ -17,12 +17,13 @@
 class registerfile : public hex
 {
 public:
-    registerfile(); //Initialize register x0 to zero, and all other registers to 0xf0f0f0f0.
-    ~registerfile(); 
+    registerfile();  //Constructor
+    ~registerfile(); //Destructor
 
-    void set(uint32_t reg, int32_t val); //Assign register r the given val. If r is zero then do nothing.
-    int32_t get(uint32_t  reg); //Return the value of register r. If r is zero then return zero.
-    void dump(const std::string &hdr); //Dump register contents.
+    void reset();                        //Initialize registers.
+    void set(uint32_t reg, int32_t val); //Set register value.
+    int32_t get(uint32_t  reg);          //Return register value.
+    void dump(const std::string &hdr);   //Dump register contents.
     
 private:
     std::vector<int32_t> regs; //Vector to simulate registers.
--- Assign5/regusterfile.cpp
+++ Assign5/regusterfile.cpp
@@ -9,3 +9,44 @@
 //
 //***************************************************************************
 #include "registerfile.h"
+
+registerfile::registerfile()
+{
+    regs.resize(32);
+    reset();
+}
+
+
+/**
+ * @brief Destroy the register vector.
+ * 
+ * @note Redundant, done automatically at end of scope.
+ * 
+ */
+registerfile~registerfile()
+{
+
+} 
+
+void registerfile::reset()
+{
+    regs.assign(32, 0xf0f0f0f0);
+    regs.at(0) = 0; //Initialize register x0 to zero,
+}
+
+void registerfile::set(uint32_t reg, int32_t val)
+{
+    if(reg != 0) //Discard values sent to register 0.
+    {
+        regs.at(reg) = val; //Assign register the given val.
+    }
+}
+int32_t registerfile::get(uint32_t reg)
+{
+    return regs.at(reg);
+} 
+void registerfile::dump(const std::string &hdr)
+{
+    
+}    //Dump register contents.
+    
