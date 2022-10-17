41c4ac8db597f108ad315b285a578c5991a6bab7
Matt Borek <z1951125@students.niu.edu>
2022-10-17 01:37:55 -0500

    Impliment run function

--- Assign5/cpu_single_hart.cpp
+++ Assign5/cpu_single_hart.cpp
@@ -9,3 +9,36 @@
 //
 //***************************************************************************
 #include "cpu_single_hart.h"
+
+/**
+ * @brief Run simulated CPU.
+ *
+ * Simulate a CPU and run executable program.
+ * 
+ * @param exec_limit Limit of instructions to execute.
+ */
+void cpu_single_hart::run(uint64_t exec_limit)
+{
+    regs.set(2, mem.get_size()); //Set register x2 to the maximum memory size.
+    if(exec_limit == 0)
+    {
+        while(!is_halted()) //While the hardware thread isn't halted.
+        {
+            tick();
+        }
+    }
+    else
+    {
+        while(!is_halted() && (get_insn_counter() < exec_limit)) //While the hardware thread isn't halted and thread isn't as execution limit.
+        {
+            tick();
+        }
+    }
+
+    if(is_halted())
+    {
+        std::cout << get_halt_reason() << std::endl;
+    }
+
+    std::cout << get_insn_counter() << "instructions executed" << std::endl;
+}
--- Assign5/cpu_single_hart.h
+++ Assign5/cpu_single_hart.h
@@ -13,12 +13,22 @@
 //***************************************************************************
 #include "rv32i_hart.h"
 
+/**
+ * @brief Simulated Hardware Thread Class
+ * 
+ * Facilitates the creation and operation of a simulated CPU with a single hardware threads, allowing a decode/fetch/execute cycle.
+ *
+ */
 class cpu_single_hart : public rv32i_hart
 {
 public:
-    cpu_single_hart(memory &mem) : rv32i_hart(mem) {}
-    void run(uint64_t exec_limit);
-
+    /**
+     * @brief Construct a new cpu with a single hardware thread.
+     * 
+     * @param mem Size for the hardware thread object to use in initialization.
+     */
+    cpu_single_hart(memory &mem) : rv32i_hart(mem) {} //Constructor
+    void run(uint64_t exec_limit);                    //Run simulated CPU.
 };
 
 #endif
